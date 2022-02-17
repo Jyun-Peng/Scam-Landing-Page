d8fef82fe3e1ce8816c039aa6ba7291f8640df32
Jenniferlala <kekvkikn@gmail.com>
2022-02-17 23:16:50 +0800

    Start the CRA, and build basic layout.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+function Section({ children }) {
+    return <div className="max-w-4xl px-4 py-48 mx-auto">{children}</div>;
+}
+
+function Header({ children }) {
+    return <header className="w-full h-14 px-80 fixed top-0 left-0 z-10">{children}</header>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header>This is a Header</Header>
+            <div className="wrapper bg-white pt-14">
+                <Section>
+                    <div>This is a Hero</div>
+                </Section>
+                <Section>
+                    <div className="flex">
+                        <div className="flex-auto">標語1</div>
+                        <div className="flex-auto">標語2</div>
+                        <div className="flex-auto">標語3</div>
+                    </div>
+                </Section>
+                <Section>
+                    <div className="flex">
+                        <div className="flex-auto">Carousel</div>
+                        <div className="flex-auto">標題&按鈕</div>
+                    </div>
+                </Section>
+                <Section>
+                    <div className="flex">
+                        <div className="flex-auto">標題&按鈕</div>
+                        <div className="flex-auto">Carousel</div>
+                    </div>
+                </Section>
+                <Section>
+                    <div>表單</div>
+                </Section>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                black: '#333333',
+                white: '#fefefe',
+                cyan: '#00e8cc',
+            },
+        },
+    },
+    plugins: [],
+};
