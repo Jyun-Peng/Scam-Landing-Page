f27367e64411a3ec3f5c6519586bfd51a836e10f
Jenniferlala <kekvkikn@gmail.com>
2022-02-19 00:03:27 +0800

    import images with Images.js.

---
+++ src/images.js
@@ -0,0 +1,28 @@
+import imageIntro1 from './images/intros/robot.png';
+import imageIntro2 from './images/intros/clock.png';
+import imageIntro3 from './images/intros/worldwide.png';
+
+import imageCarousel1 from './images/team/group1.jpg';
+import imageCarousel2 from './images/team/group2.jpg';
+import imageCarousel3 from './images/team/group3.jpg';
+
+import imageCarousel4 from './images/presentation/presentation1.jpg';
+import imageCarousel5 from './images/presentation/presentation2.jpg';
+import imageCarousel6 from './images/presentation/presentation3.jpg';
+
+import man from './images/rich man with money.png';
+import bg from './images/Background.svg';
+
+export {
+    imageIntro1,
+    imageIntro2,
+    imageIntro3,
+    imageCarousel1,
+    imageCarousel2,
+    imageCarousel3,
+    imageCarousel4,
+    imageCarousel5,
+    imageCarousel6,
+    man,
+    bg,
+};
