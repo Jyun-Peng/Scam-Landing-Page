5d29bb3ffac05b107f47c8e19eeecc2e553da12f
Jenniferlala <kekvkikn@gmail.com>
2022-02-19 00:03:55 +0800

    Add tailwind configs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,21 @@ module.exports = {
         extend: {
             colors: {
                 black: '#333333',
+                gray: '#dddddd',
+                darkGray: '#555555',
                 white: '#fefefe',
                 cyan: '#00e8cc',
             },
             flex: {
                 half: '1 1 50%',
+            },
+            fontSize: {
+                lg: ['3rem', '1'],
+                md: ['1.5rem', '1'],
+                input: ['1.2rem', '2'],
+            },
+            height: {
+                screen: '90vh',
             },
         },
     },
