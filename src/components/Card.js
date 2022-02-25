d75962e09d21981589894e9c9184007d6f2ef92c
Jenniferlala <kekvkikn@gmail.com>
2022-02-25 20:13:06 +0800

    Add paragraph to cards.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,10 @@
+import { Paragraph } from './Text';
 export default function Card({ src, text }) {
     return (
-        <div className="flex flex-col">
-            <img src={src} alt="icon" className="w-full aspect-square mb-8" />
-            <h2 className="text-center text-lg_mobile md:text-lg font-bold">{text}</h2>
+        <div className="flex flex-col items-center cursor-pointer">
+            <img src={src} alt="icon" className="w-full aspect-square mb-4" />
+            <span className="text-center text-lg_mobile md:text-lg font-bold mb-3">{text.title}</span>
+            <Paragraph center>{text.paragraph}</Paragraph>
         </div>
     );
 }
