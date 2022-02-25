5a7015478590e129a3a4d6a380e1bd0c5c8f5163
Jenniferlala <kekvkikn@gmail.com>
2022-02-25 19:57:35 +0800

    Add center parameter to text components.

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,13 +1,17 @@
-function LargeTitle({ children }) {
-    return <h1 className={`text-xxl_mobile md:text-xxl text-white font-bold text-center`}>{children}</h1>;
+function LargeTitle({ children, center }) {
+    return (
+        <h1 className={`text-xxl_mobile md:text-xxl text-white font-bold${center ? ' text-center' : ''}`}>
+            {children}
+        </h1>
+    );
 }
 
-function MediumTitle({ children }) {
-    return <h2 className={`text-xl_mobile md:text-xl font-bold`}>{children}</h2>;
+function MediumTitle({ children, center }) {
+    return <h2 className={`text-xl_mobile md:text-xl font-bold${center ? ' text-center' : ''}`}>{children}</h2>;
 }
 
-function Paragraph({ children }) {
-    return <p className="text-para text-darkGray">{children}</p>;
+function Paragraph({ children, center }) {
+    return <p className={`text-para text-darkGray${center ? ' text-center' : ''}`}>{children}</p>;
 }
 
 export { LargeTitle, MediumTitle, Paragraph };
