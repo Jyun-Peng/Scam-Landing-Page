4868de5e4c97348b144129276dcc79b8d2f490fa
Jenniferlala <kekvkikn@gmail.com>
2022-03-01 02:19:49 +0800

    Add subtitle.

--- src/components/Text.js
+++ src/components/Text.js
@@ -10,8 +10,12 @@ function MediumTitle({ children, center }) {
     return <h2 className={`text-xl_mobile md:text-xl font-bold${center ? ' text-center' : ''}`}>{children}</h2>;
 }
 
+function SubTitle({ children, center }) {
+    return <h3 className={`text-lg_mobile md:text-lg text-darkGray ${center ? ' text-center' : ''}`}>{children}</h3>;
+}
+
 function Paragraph({ children, center }) {
     return <p className={`text-para text-darkGray${center ? ' text-center' : ''}`}>{children}</p>;
 }
 
-export { LargeTitle, MediumTitle, Paragraph };
+export { LargeTitle, MediumTitle, SubTitle, Paragraph };
