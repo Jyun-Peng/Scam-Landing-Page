653df325dec2de698153cb44e523c64615e6cd89
Jenniferlala <kekvkikn@gmail.com>
2022-03-01 02:20:27 +0800

    Add button type.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,7 @@ export default function Button({ text, onClick }) {
         <button
             onClick={onClick}
             className={`inline-block px-7 py-4 bg-red hover:bg-red_hover text-white font-bold text-[1.75rem] leading-none rounded-full`}
+            type="button"
         >
             {text}
         </button>
