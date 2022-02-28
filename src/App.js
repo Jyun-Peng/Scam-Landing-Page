5c71a6fa95d703c641f3eecc10eb5273f9359008
Jenniferlala <kekvkikn@gmail.com>
2022-03-01 02:20:43 +0800

    Add intro section.

--- src/App.js
+++ src/App.js
@@ -18,10 +18,14 @@ import {
     imageCarousel6,
 } from './images';
 
-import { LargeTitle, MediumTitle, Paragraph } from './components/Text';
+import { LargeTitle, MediumTitle, SubTitle, Paragraph } from './components/Text';
 
 function Section({ children }) {
-    return <div className="bg-white py-20">{children}</div>;
+    return (
+        <div className="bg-white py-20">
+            <DefaultContainer>{children}</DefaultContainer>
+        </div>
+    );
 }
 
 function App() {
@@ -91,72 +95,78 @@ function App() {
                     </div>
                 </div>
                 <Section>
-                    <DefaultContainer>
-                        <div className="flex flex-col items-center pt-20 gap-12">
-                            <MediumTitle>使用AI網聊助手</MediumTitle>
-                            <div className="flex md:px-6">
-                                <div className="flex-1/3 px-2 mobile:px-6 desktop:px-10">
-                                    <Card src={imageIntro1} text={cardTexts[0]} />
-                                </div>
-                                <div className="flex-1/3 px-2 mobile:px-6 desktop:px-10">
-                                    <Card src={imageIntro2} text={cardTexts[1]} />
-                                </div>
-                                <div className="flex-1/3 px-2 mobile:px-6 desktop:px-10">
-                                    <Card src={imageIntro3} text={cardTexts[2]} />
-                                </div>
+                    <div className="flex flex-col gap-12 items-center">
+                        <div className="flex flex-col items-center gap-6">
+                            <MediumTitle>AI網聊助手</MediumTitle>
+                            <SubTitle>你的感情夥伴、軍師、後盾</SubTitle>
+                        </div>
+
+                        <div className="h-[80vh] aspect-square bg-phoneImg bg-centerTop bg-contain bg-no-repeat"></div>
+                    </div>
+                </Section>
+                <Section>
+                    <div className="flex flex-col items-center gap-12">
+                        <MediumTitle>為什麼要用AI網聊助手?</MediumTitle>
+                        <div className="flex md:px-6">
+                            <div className="flex-1/3 px-2 mobile:px-6 desktop:px-10">
+                                <Card src={imageIntro1} text={cardTexts[0]} />
                             </div>
+                            <div className="flex-1/3 px-2 mobile:px-6 desktop:px-10">
+                                <Card src={imageIntro2} text={cardTexts[1]} />
+                            </div>
+                            <div className="flex-1/3 px-2 mobile:px-6 desktop:px-10">
+                                <Card src={imageIntro3} text={cardTexts[2]} />
+                            </div>
+                        </div>
+                        <Button text="開始體驗" onClick={scrollToBottom} />
+                    </div>
+                </Section>
+                <Section>
+                    <div className="flex flex-wrap mobile:flex-nowrap gap-6 mobile:gap-0">
+                        <div className="flex-full mobile:flex-half order-2 mobile:order-1">
+                            <Carousel srcList={[imageCarousel1, imageCarousel2, imageCarousel3]} />
+                        </div>
+
+                        <div className="flex-full mobile:flex-half order-1 mobile:order-2 flex flex-col justify-center items-center px-4 gap-6">
+                            <MediumTitle>愛情靠專業</MediumTitle>
+                            <Paragraph>
+                                由專業AI工程師、語言學家、心理學家共同開發的 AI
+                                網聊助手，運用網路技術彌補你社交上的不足，讓你在情場上不再弱勢。
+                            </Paragraph>
+                            <div className="hidden mobile:block">
+                                <Button text="開始體驗" onClick={scrollToBottom} />
+                            </div>
+                        </div>
+                        <div className="text-center flex-full block mobile:hidden order-3">
                             <Button text="開始體驗" onClick={scrollToBottom} />
                         </div>
-                    </DefaultContainer>
+                    </div>
                 </Section>
                 <Section>
-                    <DefaultContainer>
-                        <div className="flex flex-wrap mobile:flex-nowrap gap-6 mobile:gap-0">
-                            <div className="flex-full mobile:flex-half order-2 mobile:order-1">
-                                <Carousel srcList={[imageCarousel1, imageCarousel2, imageCarousel3]} />
-                            </div>
-
-                            <div className="flex-full mobile:flex-half order-1 mobile:order-2 flex flex-col justify-center items-center px-4 gap-6">
-                                <MediumTitle>愛情靠專業</MediumTitle>
-                                <Paragraph>
-                                    由專業AI工程師、語言學家、心理學家共同開發的 AI
-                                    網聊助手，運用網路技術彌補你社交上的不足，讓你在情場上不再弱勢。
-                                </Paragraph>
-                                <Button text="開始體驗" onClick={scrollToBottom} />
-                            </div>
-                            <div className="text-center flex-full block mobile:hidden order-3">
+                    <div className="flex flex-col mobile:flex-row gap-6 mobile:gap-0">
+                        <div className="flex-full mobile:flex-half flex flex-col justify-center items-center px-4 gap-6">
+                            <MediumTitle>再一次錯過她？</MediumTitle>
+                            <Paragraph>
+                                因為不夠主動而錯失了機會?伴侶沒有高低優劣，只有願不願意嘗試。內向、害羞、恐懼都是藉口，
+                                讓我們幫助你吧！
+                            </Paragraph>
+                            <div className="hidden mobile:block">
                                 <Button text="開始體驗" onClick={scrollToBottom} />
                             </div>
                         </div>
-                    </DefaultContainer>
+                        <div className="flex-full mobile:flex-half">
+                            <Carousel srcList={[imageCarousel4, imageCarousel5, imageCarousel6]} />
+                        </div>
+                        <div className="text-center flex-full block mobile:hidden order-3">
+                            <Button text="開始體驗" onClick={scrollToBottom} />
+                        </div>
+                    </div>
                 </Section>
                 <Section>
-                    <DefaultContainer>
-                        <div className="flex flex-col mobile:flex-row gap-6 mobile:gap-0">
-                            <div className="flex-full mobile:flex-half flex flex-col justify-center items-center px-4 gap-6">
-                                <MediumTitle>用過都說讚</MediumTitle>
-                                <Paragraph>
-                                    ：「抱歉最近被女友拉著到處跑，只有趁她在睡著了才能填寫反饋表單。我覺得這個 AI
-                                    網聊助手真的有料。等等，女友醒了，我又要忙了，就先這樣囉。 👍」
-                                </Paragraph>
-                                <Button text="開始體驗" onClick={scrollToBottom} />
-                            </div>
-                            <div className="flex-full mobile:flex-half">
-                                <Carousel srcList={[imageCarousel4, imageCarousel5, imageCarousel6]} />
-                            </div>
-                            <div className="text-center flex-full block mobile:hidden order-3">
-                                <Button text="開始體驗" onClick={scrollToBottom} />
-                            </div>
-                        </div>
-                    </DefaultContainer>
-                </Section>
-                <Section>
-                    <DefaultContainer>
-                        <div className="flex flex-col items-center gap-6" id="form">
-                            <MediumTitle>想了解更多?</MediumTitle>
-                            <Form />
-                        </div>
-                    </DefaultContainer>
+                    <div className="flex flex-col items-center gap-6" id="form">
+                        <MediumTitle>想了解更多?</MediumTitle>
+                        <Form />
+                    </div>
                 </Section>
             </div>
         </div>
