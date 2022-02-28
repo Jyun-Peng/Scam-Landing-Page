4f31f6caf8a0f1a5edd01f573d1c77200462922d
Jenniferlala <kekvkikn@gmail.com>
2022-03-01 01:49:53 +0800

    Remove cards' cursor-pointer.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import { Paragraph } from './Text';
 export default function Card({ src, text }) {
     return (
-        <div className="flex flex-col items-center cursor-pointer">
+        <div className="flex flex-col items-center">
             <img src={src} alt="icon" className="w-full aspect-square mb-4" />
             <span className="text-center text-lg_mobile md:text-lg font-bold mb-3">{text.title}</span>
             <Paragraph center>{text.paragraph}</Paragraph>
